ff9e7b385cd748e1a5fac4e1a86cf843657f2f0d
brown <1104524352@qq.com>
2017-04-26 21:59:54 +0800

    asdasdad

--- bom.js
+++ bom.js
@@ -6,6 +6,6 @@ window.$ = function () {
 
 $.bom = {
     openAtCenter : function(width,height,url){
-        window.open('','_blank',`width=${width},height=${height}` )
+        window.open('url','_blank',`width=${width},height=${height}` )
     }
 }
