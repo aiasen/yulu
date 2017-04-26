416ddc8a4846963191427d601b696b689c4f3994
brown <1104524352@qq.com>
2017-04-26 22:44:35 +0800

    s

--- bom.js
+++ bom.js
@@ -7,5 +7,10 @@ window.$ = function () {
 $.bom = {
     openAtCenter : function(width,height,url){
         window.open(`${url}`,'_blank',`width=${width},height=${height},screenX=${screen.width/2-width/2}px,screenY=${screen.height/2-height/2}px` )
+    },
+    search : function(name,value){
+        if(value === undefined){
+            return window.location.search
+        }
     }
 }
