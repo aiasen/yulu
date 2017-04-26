602ada8f3cb65400f689c23d3c4f0e118446ad5c
brown <1104524352@qq.com>
2017-04-26 22:21:07 +0800

    sdass

--- bom.js
+++ bom.js
@@ -6,6 +6,6 @@ window.$ = function () {
 
 $.bom = {
     openAtCenter : function(width,height,url){
-        window.open(`${url}`,'_blank',`width=${width},height=${height},screenX=${screen.width/2-width/s}px,screenY=${screen.height/2-height/2}px` )
+        window.open(`${url}`,'_blank',`width=${width},height=${height},screenX=${screen.width/2-width/2}px,screenY=${screen.height/2-height/2}px` )
     }
 }
