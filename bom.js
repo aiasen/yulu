e76673aa8290cff55b84b1e26c751bd94e96dbb7
brown <1104524352@qq.com>
2017-04-27 19:00:14 +0800

    asd

--- bom.js
+++ bom.js
@@ -9,7 +9,7 @@ $.bom = {
         window.open(`${url}`, '_blank', `width=${width},height=${height},screenX=${screen.width / 2 - width / 2}px,screenY=${screen.height / 2 - height / 2}px`)
     },
     search: function (name, value) {
-        if (value === undefined) {
+        let searchAll = function () {
             let result = {}
             let search = window.location.search
             if (search[0] === '?') {
@@ -18,12 +18,26 @@ $.bom = {
             let searchArray = search.split('&')
             for (var i = 0; i < searchArray.length; i++) {
                 let parts = searchArray[i].split('=')
-                result[decodeURIComponent(parts[0])] = decodeURIComponent(parts[1])
+                result[decodeURIComponent(parts[0])] = decodeURIComponent(parts[1] || '')
             }
+            return result
+        }
+        let result = searchAll()
+        if (value === undefined) {
+
             return result[name]
+
         } else {
-            let search = window.location.search
-            return search.indexOf(`${name}=`)
+            if (result[name] === undefined) {
+                location.search += `&${encodeURIComponent(name)}=${encodeURIComponent(value)}`
+            } else {
+                result[name] = encodeURIComponent(value)
+                let newSearch = '?'
+                for (let key in result) {
+                    newSearch += `${encodeURIComponent(key)}=${encodeURIComponent(result[key])}&`
+                }
+                location.search = newSearch
+            }
         }
     }
 }
